fb58da678ca71a400c9fddc39a62696132eb20d3
Jeimy Sanchez <jeimyestefanias@gmail.com>
2026-04-05 19:44:27 -0500

    modelo inicial pet

---
+++ app/src/main/java/com/example/petcarecjc/model/Pet.kt
@@ -0,0 +1,9 @@
+package com.example.petcarecjc.model
+
+data class Pet(
+    var id: String = "",
+    var nombre: String = "",
+    var tipo: String = "",
+    var genero: String = "",
+    var descripcion: String = ""
+)
