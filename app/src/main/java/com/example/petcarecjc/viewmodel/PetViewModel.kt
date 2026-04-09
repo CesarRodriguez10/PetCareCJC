5db494fa37dd85b5ba3ba2e33b00e12603e45d99
MariaCamilaPalacio <mariacamilapalacioromero@gmail.com>
2026-04-09 18:58:26 -0500

    creacion del view model

---
+++ app/src/main/java/com/example/petcarecjc/viewmodel/PetViewModel.kt
@@ -0,0 +1,17 @@
+package com.example.petcarecjc.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.example.petcarecjc.model.Pet
+import com.example.petcarecjc.repository.PetRepository
+
+class PetViewModel : ViewModel() {
+
+    private val repository = PetRepository()
+
+    fun savePet(pet: Pet, onResult: (String) -> Unit) {
+        repository.savePet(pet,
+            onSuccess = { onResult("Guardado") },
+            onError = { onResult("Error: ${it.message}") }
+        )
+    }
+}
