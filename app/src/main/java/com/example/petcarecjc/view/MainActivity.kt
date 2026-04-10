cd7d9e71bf4c78f00ced1a151faa53fe4a35571c
CesarRodriguez10 <alejorz2004@gmail.com>
2026-04-09 21:43:12 -0500

    Codigo completo del MainActivity

---
+++ app/src/main/java/com/example/petcarecjc/view/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.example.petcarecjc.view
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            MainScreen {
+                startActivity(Intent(this, RegisterPetActivity::class.java))
+            }
+        }
+    }
+}
+
+@Composable
+fun MainScreen(onStartClick: () -> Unit) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(24.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Text(
+            text = "Bienvenido a PetCare CJC 🐾",
+            style = MaterialTheme.typography.headlineMedium
+        )
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        Button(onClick = { onStartClick() }) {
+            Text("Comenzar")
+        }
+    }
+}
