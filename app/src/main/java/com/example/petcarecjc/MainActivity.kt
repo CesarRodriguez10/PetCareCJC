c0f244df2c46b3491193fde7e1d6b967836aa3a5
Jeimy Sanchez <jeimyestefanias@gmail.com>
2026-04-15 19:56:45 -0500

    Diseño pantalla 1

--- app/src/main/java/com/example/petcarecjc/MainActivity.kt
+++ app/src/main/java/com/example/petcarecjc/MainActivity.kt
@@ -1,29 +1,28 @@
 package com.example.petcarecjc
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import com.example.petcarecjc.ui.theme.PetCareCJCTheme
+import com.example.petcarecjc.view.RegisterPetActivity
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
         setContent {
             PetCareCJCTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                MainScreen {
+                    startActivity(Intent(this, RegisterPetActivity::class.java))
                 }
             }
         }
@@ -31,17 +30,34 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun MainScreen(onStartClick: () -> Unit) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(24.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PetCareCJCTheme {
-        Greeting("Android")
+        Text("Bienvenido a PetCare CJC!", style = MaterialTheme.typography.headlineMedium)
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        Image(
+            painter = painterResource(id = R.drawable.pets),
+            contentDescription = null,
+            modifier = Modifier.size(200.dp)
+        )
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        Text("Cuidémoslos juntos")
+        Text("Descubre como cuidar a tus mascotas")
+
+        Spacer(modifier = Modifier.height(30.dp))
+
+        Button(onClick = onStartClick) {
+            Text("Comenzar")
+        }
     }
 }
