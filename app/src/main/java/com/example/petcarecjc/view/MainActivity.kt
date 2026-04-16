b6ead28f5dd5918092133d77b071df355d763b8d
Jeimy Sanchez <jeimyestefanias@gmail.com>
2026-04-15 21:29:03 -0500

    Diseño pantalla 1 final

--- app/src/main/java/com/example/petcarecjc/view/MainActivity.kt
+++ app/src/main/java/com/example/petcarecjc/view/MainActivity.kt
@@ -4,12 +4,18 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import com.example.petcarecjc.R
 
 class MainActivity : ComponentActivity() {
 
@@ -26,22 +32,44 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainScreen(onStartClick: () -> Unit) {
+
     Column(
         modifier = Modifier
             .fillMaxSize()
             .padding(24.dp),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
     ) {
 
         Text(
-            text = "Bienvenido a PetCare CJC 🐾",
+            text = "Bienvenido a \nPetCare CJC",
             style = MaterialTheme.typography.headlineMedium
         )
 
         Spacer(modifier = Modifier.height(20.dp))
 
-        Button(onClick = { onStartClick() }) {
+        Image(
+            painter = painterResource(id = R.drawable.pets),
+            contentDescription = "Mascotas",
+            modifier = Modifier
+                .size(220.dp)
+                .clip(RoundedCornerShape(20.dp))
+        )
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        Text(
+            text = "Cuidémolos juntos",
+            style = MaterialTheme.typography.headlineSmall
+        )
+        Text("Descubre como cuidar a tus mascotas")
+
+        Spacer(modifier = Modifier.height(30.dp))
+
+        Button(
+            onClick = onStartClick,
+            modifier = Modifier.fillMaxWidth()
+        ) {
             Text("Comenzar")
         }
     }
