b26eebe33bb60a9e802b51a024b45963aedf8427
Jeimy Sanchez <jeimyestefanias@gmail.com>
2026-05-28 22:09:36 -0500

    correccion codigo para traduccion

--- app/src/main/java/com/example/petcarecjc/view/PetList.kt
+++ app/src/main/java/com/example/petcarecjc/view/PetList.kt
@@ -22,16 +22,22 @@ fun PetListScreen(
     Column {
 
         TextButton(onClick = onBack) {
-            Text(stringResource(R.string.back))
+            Text(
+                stringResource(R.string.back)
+            )
         }
 
         OutlinedTextField(
             value = search,
             onValueChange = { search = it },
-            placeholder = { Text(stringResource(R.string.search_pet)) }
+            placeholder = { Text(
+                stringResource(R.string.search_pet)
+            ) }
         )
 
-        Text(stringResource(R.string.my_pets))
+        Text(
+            stringResource(R.string.my_pets)
+        )
 
         LazyColumn {
             items(pets) { pet ->
@@ -48,10 +54,18 @@ fun PetItem(pet: Pet, onDetailClick: (Pet) -> Unit) {
         modifier = Modifier.fillMaxWidth(),
         horizontalArrangement = Arrangement.SpaceBetween
     ) {
-        Text("${pet.nombre} - ${pet.tipo}")
+        Text(
+            stringResource(
+                R.string.pet_item_format,
+                pet.nombre,
+                pet.tipo
+            )
+        )
 
         Button(onClick = { onDetailClick(pet) }) {
-            Text(stringResource(R.string.detail))
+            Text(
+                stringResource(R.string.detail)
+            )
         }
     }
 }
--- app/src/main/java/com/example/petcarecjc/view/RegisterPetActivity.kt
+++ app/src/main/java/com/example/petcarecjc/view/RegisterPetActivity.kt
@@ -2,73 +2,385 @@ package com.example.petcarecjc.view
 
 import android.os.Bundle
 import android.widget.Toast
-import androidx.activity.*
+import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.compose.foundation.*
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
-import androidx.compose.ui.*
-import androidx.compose.ui.res.*
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import com.example.petcarecjc.R
 import com.example.petcarecjc.model.Pet
 import com.example.petcarecjc.viewmodel.PetViewModel
+import kotlinx.coroutines.delay
+import android.net.Uri
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import coil.compose.rememberAsyncImagePainter
+
 
 class RegisterPetActivity : ComponentActivity() {
 
     private val viewModel: PetViewModel by viewModels()
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
+
             val context = LocalContext.current
 
             RegisterPetScreen(
                 onSave = { pet ->
+
                     viewModel.savePet(pet) {
-                        Toast.makeText(context, it, Toast.LENGTH_SHORT).show()
+                        Toast.makeText(
+                            context,
+                            it,
+                            Toast.LENGTH_SHORT
+                        ).show()
                     }
                 },
-                onBack = { finish() }
+
+                onBack = {
+                    finish()
+                }
             )
         }
     }
 }
-//registro de mascotas
+
 @Composable
-fun RegisterPetScreen(onSave: (Pet) -> Unit, onBack: () -> Unit) {
+fun RegisterPetScreen(
+    onSave: (Pet) -> Unit,
+    onBack: () -> Unit
+) {
 
     var name by remember { mutableStateOf("") }
     var type by remember { mutableStateOf("") }
+    var breed by remember { mutableStateOf("") }
+    var gender by remember { mutableStateOf("") }
+    var age by remember { mutableStateOf("") }
+    var description by remember { mutableStateOf("") }
+    var vaccines by remember { mutableStateOf("") }
+    var diseases by remember { mutableStateOf("") }
+    var medications by remember { mutableStateOf("") }
+    var allergies by remember { mutableStateOf("") }
+    var lastVisit by remember { mutableStateOf("") }
+    var showSavedCard by remember { mutableStateOf(false) }
+    val scrollState = rememberScrollState()
+    var selectedImageUri by remember { mutableStateOf<Uri?>(null) }
 
-    Column(Modifier.padding(20.dp)) {
+    val launcher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.GetContent()
+    ) { uri: Uri? ->
 
-        TextButton(onClick = onBack) {
-            Text(stringResource(R.string.back))
+        selectedImageUri = uri
+    }
+    LaunchedEffect(showSavedCard) {
+
+        if (showSavedCard) {
+
+            delay(3000)
+            showSavedCard = false
+        }
+    }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .verticalScroll(scrollState)
+    ) {
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(10.dp)
+        ) {
+
+            TextButton(
+                onClick = onBack
+            ) {
+
+                Text(
+                    stringResource(R.string.back)
+                )
+            }
         }
 
-        Text(stringResource(R.string.add_pet))
 
-        CustomTextField(name, { name = it }, stringResource(R.string.name))
-        CustomTextField(type, { type = it }, "Tipo")
-
-        Button(
-            onClick = {
-                onSave(Pet(nombre = name, tipo = type))
-            },
+        Box(
             modifier = Modifier.fillMaxWidth()
         ) {
-            Text(stringResource(R.string.save_pet))
+
+            Image(
+                painter = painterResource(id = R.drawable.jager),
+
+                contentDescription = stringResource(R.string.pet_image),
+
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(250.dp)
+                    .clip(
+                        RoundedCornerShape(
+                            bottomStart = 20.dp,
+                            bottomEnd = 20.dp
+                        )
+                    ),
+
+                contentScale = ContentScale.Crop
+            )
+
+            Button(
+                onClick = { launcher.launch("image/*") },
+
+                modifier = Modifier
+                    .padding(16.dp)
+                    .align(Alignment.BottomEnd),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor =
+                        MaterialTheme.colorScheme
+                            .primaryContainer
+                            .copy(alpha = 0.8f)
+                )
+            ) {
+
+                Text(
+                    text = stringResource(R.string.change_photo),
+
+                    color =
+                        MaterialTheme.colorScheme
+                            .onPrimaryContainer
+                )
+            }
+        }
+
+        Column(
+            modifier = Modifier.padding(20.dp)
+        ) {
+
+            Text(
+                text = stringResource(R.string.add_pet),
+                style = MaterialTheme.typography.headlineMedium
+            )
+
+            if (showSavedCard) {
+
+                Card(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(vertical = 10.dp),
+
+                    shape = RoundedCornerShape(16.dp)
+                ) {
+
+                    Column(
+                        modifier = Modifier.padding(16.dp)
+                    ) {
+
+                        Text(
+                            text = stringResource(R.string.pets_saved)
+                        )
+
+                        TextButton(
+                            onClick = {
+                                showSavedCard = false
+                            }
+                        ) {
+
+                            Text("🐾")
+                        }
+                    }
+                }
+            }
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Text(
+                text = stringResource(R.string.personal_info),
+
+                fontWeight = FontWeight.Bold,
+
+                color = MaterialTheme.colorScheme.primary
+            )
+
+            HorizontalDivider(
+                modifier = Modifier.padding(vertical = 8.dp)
+            )
+
+            CustomTextField(
+                value = name,
+                onValueChange = { name = it },
+                label = stringResource(R.string.name)
+            )
+
+            CustomTextField(
+                value = type,
+                onValueChange = { type = it },
+                label = stringResource(R.string.type)
+            )
+
+            CustomTextField(
+                value = breed,
+                onValueChange = { breed = it },
+                label = stringResource(R.string.breed)
+            )
+
+            CustomTextField(
+                value = gender,
+                onValueChange = { gender = it },
+                label = stringResource(R.string.gender)
+            )
+
+            CustomTextField(
+                value = age,
+                onValueChange = { age = it },
+                label = stringResource(R.string.age)
+            )
+
+            CustomTextField(
+                value = description,
+                onValueChange = { description = it },
+                label = stringResource(R.string.description)
+            )
+
+            Spacer(modifier = Modifier.height(24.dp))
+
+            Text(
+                text = stringResource(R.string.health_info),
+
+                fontWeight = FontWeight.Bold,
+
+                color = MaterialTheme.colorScheme.primary
+            )
+
+            HorizontalDivider(
+                modifier = Modifier.padding(vertical = 8.dp)
+            )
+
+            CustomTextField(
+                value = vaccines,
+                onValueChange = { vaccines = it },
+                label = stringResource(R.string.vaccines)
+            )
+
+            CustomTextField(
+                value = diseases,
+                onValueChange = { diseases = it },
+                label = stringResource(R.string.diseases)
+            )
+
+            CustomTextField(
+                value = medications,
+                onValueChange = { medications = it },
+                label = stringResource(R.string.medications)
+            )
+
+            CustomTextField(
+                value = allergies,
+                onValueChange = { allergies = it },
+                label = stringResource(R.string.allergies)
+            )
+
+            CustomTextField(
+                value = lastVisit,
+                onValueChange = { lastVisit = it },
+                label = stringResource(R.string.last_visit)
+            )
+
+            Spacer(modifier = Modifier.height(30.dp))
+
+            Button(
+
+                onClick = {
+
+                    val pet = Pet(
+                        nombre = name,
+                        tipo = type,
+                        raza = breed,
+                        genero = gender,
+                        edad = age,
+                        descripcion = description,
+                        vacunas = vaccines,
+                        enfermedades = diseases,
+                        medicamentos = medications,
+                        alergias = allergies,
+                        ultimaConsulta = lastVisit
+                    )
+
+                    onSave(pet)
+
+                    showSavedCard = true
+
+                    name = ""
+                    type = ""
+                    breed = ""
+                    gender = ""
+                    age = ""
+                    description = ""
+
+                    vaccines = ""
+                    diseases = ""
+                    medications = ""
+                    allergies = ""
+                    lastVisit = ""
+                },
+
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(55.dp),
+
+                shape = RoundedCornerShape(16.dp)
+            ) {
+
+                Text(
+                    text = stringResource(R.string.save_pet),
+
+                    style = MaterialTheme.typography.titleMedium
+                )
+            }
+
+            Spacer(
+                modifier = Modifier.height(40.dp)
+            )
         }
     }
 }
 
 @Composable
-fun CustomTextField(value: String, onValueChange: (String) -> Unit, label: String) {
-    OutlinedTextField(value, onValueChange, label = { Text(label) })
+fun CustomTextField(
+    value: String,
+    onValueChange: (String) -> Unit,
+    label: String
+) {
+
+    OutlinedTextField(
+
+        value = value,
+
+        onValueChange = onValueChange,
+
+        label = {
+            Text(label)
+        },
+
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 4.dp),
+
+        shape = RoundedCornerShape(12.dp)
+    )
 }
