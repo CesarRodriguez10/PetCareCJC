1a4f20950d7bdfa6a42807f6eefd050ebde5600f
MariaCamilaPalacio <mariacamilapalacioromero@gmail.com>
2026-06-03 07:24:25 -0500

    correccion2.0

--- app/src/main/java/com/example/petcarecjc/view/RemindersActivity.kt
+++ app/src/main/java/com/example/petcarecjc/view/RemindersActivity.kt
@@ -2,13 +2,11 @@ package com.example.petcarecjc.view
 
 import android.app.DatePickerDialog
 import android.app.TimePickerDialog
-import android.os.Build
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.annotation.RequiresApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
@@ -37,7 +35,6 @@ class RemindersActivity : ComponentActivity() {
 
     private val viewModel: ReminderViewModel by viewModels()
 
-    @RequiresApi(Build.VERSION_CODES.S)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val petId   = intent.getStringExtra("petId")   ?: ""
@@ -48,7 +45,6 @@ class RemindersActivity : ComponentActivity() {
     }
 }
 
-@RequiresApi(Build.VERSION_CODES.S)
 @Composable
 fun RemindersScreen(petId: String, petName: String, viewModel: ReminderViewModel, onBack: () -> Unit) {
     val context = LocalContext.current
